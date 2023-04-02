f5cf24b447c7b945f79b1e1a2bbdb987ad9267da
peterouob <hellolinpeter@gmail.com>
2023-04-03 05:09:54 +0800

    使用vueRouter來避免element-plus中navbar的高亮坑

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,38 @@
 <template>
-  <div>
-    <h1>Home</h1>
-  </div>
+    <div class="navbar" >
+      <el-menu :default-active="activeIndex" mode="horizontal" >
+        <el-menu-item index="5"><RouterLink to="/home" class="a">主頁</RouterLink></el-menu-item>
+        <el-menu-item index="1" ><RouterLink to="/bag" class="a">背包</RouterLink></el-menu-item>
+        <el-menu-item index="2"><RouterLink to="/clue" class="a">線索</RouterLink></el-menu-item>
+        <el-menu-item index="3"><RouterLink to="/map" class="a">地圖</RouterLink></el-menu-item>
+        <el-menu-item index="4"><RouterLink to="/any" class="a">其他</RouterLink></el-menu-item>
+      </el-menu>
+    </div>
+  <RouterView/>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import {Ref,ref} from "vue";
+import {RouterView,RouterLink,useRouter} from 'vue-router'
+
+const {currentRoute } = useRouter()
+
+const activeIndex : Ref<String> = ref<string>(currentRoute.value.path.split('/')[1])
+</script>
+
+<style scoped>
+.navbar {
+  padding: 0 20px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  width: 100%;
+}
+.a{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  font-size: 1.25rem;
+  width: 4rem;
+  height: 100%;
+}
+</style>
