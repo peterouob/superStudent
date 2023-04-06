aa404a50a94f4482e943f17075119bd3ab531e06
peterouob <hellolinpeter@gmail.com>
2023-04-06 16:07:52 +0800

    新增路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,38 @@
 import {createRouter,createWebHistory,RouterOptions,Router,RouteRecordRaw} from "vue-router";
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import Bag from '../views/Bag.vue'
+import Clue from '../views/Clue.vue'
+import Map from '../views/Map.vue'
+import Any from '../views/Any.vue'
+
 const routes : RouteRecordRaw[]=[
     {
         path:'/home',
         name:'home',
-        component:Home
+        component:Home,
+        children:[
+            {
+                path:'/bag',
+                name:'bag',
+                component:Bag
+            },
+            {
+                path:'/clue',
+                name:'clue',
+                component:Clue
+            },
+            {
+                path:'/map',
+                name:'map',
+                component:Map
+            },
+            {
+                path:'/any',
+                name:'any',
+                component:Any
+            }
+        ]
     },
     {
         path:'/',
